f136957c542eecf26d5d71e49c29753134c3f8ac
caganze <caganze@gmail.com>
2018-07-03 22:03:47 -0700

    changed test

--- shapes/__init__.py
+++ shapes/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+__all__=['shapes', 'selector']
 from .shapes import *
 from .selector import*
--- shapes/tests/test_shapes.py
+++ shapes/tests/test_shapes.py
@@ -9,12 +9,10 @@ x=np.random.random(100)
 y=np.random.random(100)
 
 df=pd.DataFrame([x, y]).transpose()
-
 df.columns=['x', 'y']
-
 def test_box():
 	b1=shapes.Box()
-	b1.data=np.array([x,y])
+	b1.data=df
 	b2=copy.deepcopy(b1)
 	b2.rotate(np.pi/2)
 	b3=copy.deepcopy(b1)
