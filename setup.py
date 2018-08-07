18269f6af17acab82003fc6838bb3c0c7b36d98b
caganze <caganze@gmail.com>
2018-08-07 13:15:50 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='shapes',
+      version='1.0',
+      description='python code for my research',
+      author='Christian Aganze',
+      author_email='caganze@gmail.com',
+      url='',
+      packages=['shapes'],
+     )
