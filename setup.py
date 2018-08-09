5669d6040ee0b1eb44a10a9baff80c339328a56f
caganze <caganze@gmail.com>
2018-08-08 21:28:03 -0700

    modified requirements

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setuptools.setup(
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+        "Operating System :: OS Independent"),
+)
