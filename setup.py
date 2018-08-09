2cba3cc1c3da33307a890aaee6c17d617b319d92
caganze <caganze@gmail.com>
2018-08-08 22:14:01 -0700

    cleaned

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="coolshapes",
+    name="wispshapes",
     version="0.0.1",
     author="caganze",
     author_email="caganze@gmail.com",
