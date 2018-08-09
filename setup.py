66be80f4cc7ccb0a565c5fbd469fdb4c2f78b88e
caganze <caganze@gmail.com>
2018-08-08 22:04:30 -0700

    renamed directory

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="shapes",
+    name="wisp_shapes",
     version="0.0.1",
     author="caganze",
     author_email="caganze@gmail.com",
