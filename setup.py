44de1e1c05b52ddc1a5c40225a162ce624ac93d5
caganze <caganze@gmail.com>
2018-08-08 22:11:01 -0700

    renamed directory because pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wisp_shapes",
+    name="coolshapes",
     version="0.0.1",
     author="caganze",
     author_email="caganze@gmail.com",
